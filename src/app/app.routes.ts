3c4c87e61d8f4c54e702c2f782abd1e34836f2bc
locvosts <loc.vo@saigontechnology.com>
2024-03-17 13:48:20 +0700

    Add Material Lib and Routing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { DetailComponent } from './pages/detail/detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'detail/:id', component: DetailComponent, pathMatch: 'full' },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: '**', redirectTo: '' },
+];
